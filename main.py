0af12eed788830b81533091d6b1483fc1195dc2b
Kirtan Bhavsar <128965245+KirtanBhavsar8888@users.noreply.github.com>
2024-10-03 22:55:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,261 @@
+# Importing libraries
+from flask import Flask, render_template, request, flash, redirect, url_for
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+import math, random
+from datetime import datetime
+import datetime as dt
+import yfinance as yf
+import tweepy
+import preprocessor as p
+import re
+from sklearn.linear_model import LinearRegression
+from textblob import TextBlob
+import constants as ct
+from Tweet import Tweet
+import nltk
+nltk.download('punkt')
+
+import warnings
+warnings.filterwarnings("ignore")
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# Initialize the Flask app
+app = Flask(__name__)
+# Run the flask app
+
+# To control caching so as to save and retrieve plot figs on client side
+@app.after_request
+def add_header(response):
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Expires'] = '0'
+    return response
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/insertintotable',methods=['POST'])
+def insertintotable():
+    nm = request.form['nm']
+
+    # Functions to fetch data
+    def get_historical(quote):
+        end = datetime.now()
+        start = datetime(end.year - 2, end.month, end.day)
+        data = yf.download(quote, start=start, end=end)
+        df = pd.DataFrame(data=data)
+        df.to_csv(''+quote+'.csv')
+
+        if(df.empty):
+            ts = TimeSeries(key = 'L5QMJKNDPH5FNCGO',output_format='pandas')
+            data, meta_data = ts.get_daily_adjusted(symbol='NSE:'+quote, outputsize='full')
+            #Format df
+            #Last 2 yrs rows => 502, in ascending order => ::-1
+            data=data.head(503).iloc[::-1]
+            data=data.reset_index()
+            #Keep Required cols only
+            df=pd.DataFrame()
+            df['Date']=data['date']
+            df['Open']=data['1. open']
+            df['High']=data['2. high']
+            df['Low']=data['3. low']
+            df['Close']=data['4. close']
+            df['Adj Close']=data['5. adjusted close']
+            df['Volume']=data['6. volume']
+            df.to_csv(''+quote+'.csv',index=False)
+        return
+    
+    # ARIMA Section
+    def ARIMA_ALGO(df):
+        uniqueVals = df["Code"].unique()  
+        len(uniqueVals)
+        df=df.set_index("Code")
+        #for daily basis
+        def parser(x):
+            return datetime.strptime(x, '%Y-%m-%d')
+        def arima_model(train, test):
+            history = [x for x in train]
+            predictions = list()
+            for t in range(len(test)):
+                model = ARIMA(history, order=(5,1,0))
+                model_fit = model.fit(disp=0)
+                output = model_fit.forecast()
+                yhat = output[0]
+                predictions.append(yhat)
+                obs = test[t]
+                history.append(obs)
+            return predictions
+        for company in uniqueVals[:10]:
+            data=(df.loc[company,:]).reset_index()
+            data['Price'] = data['Close']
+            Quantity_date = data[['Price','Date']]
+            Quantity_date.index = Quantity_date['Date'].map(lambda x: parser(x))
+            Quantity_date['Price'] = Quantity_date['Price'].map(lambda x: float(x))
+            Quantity_date = Quantity_date.fillna(Quantity_date.bfill())
+            Quantity_date = Quantity_date.drop(['Date'],axis =1)
+            fig = plt.figure(figsize=(7.2,4.8),dpi=65)
+            plt.plot(Quantity_date)
+            plt.savefig('static/Trends.png')
+            plt.close(fig)
+
+            quantity = Quantity_date.values
+            size = int(len(quantity) * 0.80)
+            train, test = quantity[0:size], quantity[size:len(quantity)]
+            #fit in model
+            predictions = arima_model(train, test)
+
+            fig = plt.figure(figsize=(7.2,4.8),dpi=65)
+            plt.plot(test,label='Actual Price')
+            plt.plot(predictions,label='Predicted Price')
+            plt.legend(loc=4)
+            plt.savefig('static/ARIMA.png')
+            plt.close(fig)
+
+            print("##############################################################################")
+            arima_pred=predictions[-2]
+            print("Tomorrow's",quote," Closing Price Prediction by ARIMA:",arima_pred)
+
+            error_arima = math.sqrt(mean_squared_error(test, predictions))
+            print("ARIMA RMSE:",error_arima)
+            print("##############################################################################")
+            return arima_pred, error_arima
+        
+    # LSTM Section
+    def LSTM_ALGO(df):
+        #Split data into training set and test set
+        dataset_train=df.iloc[0:int(0.8*len(df)),:]
+        dataset_test=df.iloc[int(0.8*len(df)):,:]
+        ############# NOTE #################
+        #TO PREDICT STOCK PRICES OF NEXT N DAYS, STORE PREVIOUS N DAYS IN MEMORY WHILE TRAINING
+        # HERE N=7
+        ###dataset_train=pd.read_csv('Google_Stock_Price_Train.csv')
+        training_set=df.iloc[:,4:5].values# 1:2, to store as numpy array else Series obj will be stored
+        #select cols using above manner to select as float64 type, view in var explorer
+
+        #Feature Scaling
+        from sklearn.preprocessing import MinMaxScaler
+        sc=MinMaxScaler(feature_range=(0,1))#Scaled values btween 0,1
+        training_set_scaled=sc.fit_transform(training_set)
+        #In scaling, fit_transform for training, transform for test
+        
+        #Creating data stucture with 7 timesteps and 1 output. 
+        #7 timesteps meaning storing trends from 7 days before current day to predict 1 next output
+        X_train=[]#memory with 7 days from day i
+        y_train=[]#day i
+        for i in range(7,len(training_set_scaled)):
+            X_train.append(training_set_scaled[i-7:i,0])
+            y_train.append(training_set_scaled[i,0])
+        #Convert list to numpy arrays
+        X_train=np.array(X_train)
+        y_train=np.array(y_train)
+        X_forecast=np.array(X_train[-1,1:])
+        X_forecast=np.append(X_forecast,y_train[-1])
+        #Reshaping: Adding 3rd dimension
+        X_train=np.reshape(X_train, (X_train.shape[0],X_train.shape[1],1))#.shape 0=row,1=col
+        X_forecast=np.reshape(X_forecast, (1,X_forecast.shape[0],1))
+        #For X_train=np.reshape(no. of rows/samples, timesteps, no. of cols/features)
+        
+        #Building RNN
+        from keras.models import Sequential
+        from keras.layers import Dense
+        from keras.layers import Dropout
+        from keras.layers import LSTM
+        
+        #Initialise RNN
+        regressor=Sequential()
+        
+        #Add first LSTM layer
+        regressor.add(LSTM(units=50,return_sequences=True,input_shape=(X_train.shape[1],1)))
+        #units=no. of neurons in layer
+        #input_shape=(timesteps,no. of cols/features)
+        #return_seq=True for sending recc memory. For last layer, retrun_seq=False since end of the line
+        regressor.add(Dropout(0.1))
+        
+        #Add 2nd LSTM layer
+        regressor.add(LSTM(units=50,return_sequences=True))
+        regressor.add(Dropout(0.1))
+        
+        #Add 3rd LSTM layer
+        regressor.add(LSTM(units=50,return_sequences=True))
+        regressor.add(Dropout(0.1))
+        
+        #Add 4th LSTM layer
+        regressor.add(LSTM(units=50))
+        regressor.add(Dropout(0.1))
+        
+        #Add o/p layer
+        regressor.add(Dense(units=1))
+        
+        #Compile
+        regressor.compile(optimizer='adam',loss='mean_squared_error')
+        
+        #Training
+        regressor.fit(X_train,y_train,epochs=25,batch_size=32 )
+        #For lstm, batch_size=power of 2
+        
+        #Testing
+        ###dataset_test=pd.read_csv('Google_Stock_Price_Test.csv')
+        real_stock_price=dataset_test.iloc[:,4:5].values
+        
+        #To predict, we need stock prices of 7 days before the test set
+        #So combine train and test set to get the entire data set
+        dataset_total=pd.concat((dataset_train['Close'],dataset_test['Close']),axis=0) 
+        testing_set=dataset_total[ len(dataset_total) -len(dataset_test) -7: ].values
+        testing_set=testing_set.reshape(-1,1)
+        #-1=till last row, (-1,1)=>(80,1). otherwise only (80,0)
+        
+        #Feature scaling
+        testing_set=sc.transform(testing_set)
+        
+        #Create data structure
+        X_test=[]
+        for i in range(7,len(testing_set)):
+            X_test.append(testing_set[i-7:i,0])
+            #Convert list to numpy arrays
+        X_test=np.array(X_test)
+        
+        #Reshaping: Adding 3rd dimension
+        X_test=np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
+        
+        #Testing Prediction
+        predicted_stock_price=regressor.predict(X_test)
+        
+        #Getting original prices back from scaled values
+        predicted_stock_price=sc.inverse_transform(predicted_stock_price)
+        fig = plt.figure(figsize=(7.2,4.8),dpi=65)
+        plt.plot(real_stock_price,label='Actual Price')  
+        plt.plot(predicted_stock_price,label='Predicted Price')
+          
+        plt.legend(loc=4)
+        plt.savefig('static/LSTM.png')
+        plt.close(fig)
+        
+        
+        error_lstm = math.sqrt(mean_squared_error(real_stock_price, predicted_stock_price))
+        
+        
+        #Forecasting Prediction
+        forecasted_stock_price=regressor.predict(X_forecast)
+        
+        #Getting original prices back from scaled values
+        forecasted_stock_price=sc.inverse_transform(forecasted_stock_price)
+        
+        lstm_pred=forecasted_stock_price[0,0]
+        print()
+        print("##############################################################################")
+        print("Tomorrow's ",quote," Closing Price Prediction by LSTM: ",lstm_pred)
+        print("LSTM RMSE:",error_lstm)
+        print("##############################################################################")
+        return lstm_pred,error_lstm
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
